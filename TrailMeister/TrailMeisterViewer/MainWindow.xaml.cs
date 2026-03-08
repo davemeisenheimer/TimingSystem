50b850794ee6a0f54bae4b9b5cbdadcf3e66bc85
Dave Meisenheimer <davemeisenheimer@hotmail.com>
2026-03-08 11:09:35 -0400

    Break viewer tabs into their own files

--- TrailMeister/TrailMeisterViewer/MainWindow.xaml.cs
+++ TrailMeister/TrailMeisterViewer/MainWindow.xaml.cs
@@ -1,11 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-
-using TrailMeisterViewer.Windows.EventViewer;
-using TrailMeisterDb;
-using System.ComponentModel;
-using System;
 
 namespace TrailMeisterViewer
 {
@@ -20,26 +13,6 @@ namespace TrailMeisterViewer
             this._viewModel = new MainWindowVM(this);
             this.DataContext = this._viewModel;
             InitializeComponent();
-            SetInitialSortOrder();
         }
-
-        private void OnEventRowDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            e.Handled = true;
-            var row = (DataGridRow)sender;
-            DbEvent dbEvent = (DbEvent)(row.Item);
-
-            EventViewerController eventViewerController = new EventViewerController(dbEvent);
-            eventViewerController.ShowWindow();
-
-            // FixMe: This is ugly and likely a bad pattern.
-            this._viewModel.RelayCommand_Refresh.Execute(dbEvent);
-        }
-        private void SetInitialSortOrder()
-        {
-            gridEvents.Items.SortDescriptions.Clear();
-            gridEvents.Items.SortDescriptions.Add(new SortDescription(nameof(DbEvent.EventDate), ListSortDirection.Descending));
-        }
-
     }
 }
---
+++ TrailMeister/TrailMeisterViewer/Windows/MainWindow/PeopleTab.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrailMeisterViewer.Windows.MainWindow
+{
+    /// <summary>
+    /// Interaction logic for PeopleTab.xaml
+    /// </summary>
+    public partial class PeopleTab : UserControl
+    {
+        public PeopleTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
